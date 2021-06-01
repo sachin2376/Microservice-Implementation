5655190197b611940dadf3739d8d9e36265adb81
Sachin <sachin23698@gmail.com>
2021-06-01 21:14:18 +0530

    Final commit

---
+++ MicroservicesClient/MicroservicesClient/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MicroservicesClient.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace MicroservicesClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+        private HttpClient httpClient;
+
+        public HomeController(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        
+
+        public async Task<ActionResult> Index()
+        {
+            var response = await httpClient.GetAsync("http://localhost:8080/api/Employee");
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            var employees = new List<Employee>();
+            if(response.Content.Headers.ContentType.MediaType == "application/json")
+            {
+                employees = JsonConvert.DeserializeObject<List<Employee>>(content);
+            }
+            return View(employees);
+        }
+
+        public  ActionResult UpdateEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateEmployee(Employee emp)
+        {
+            var response = await
+        }
+
+        
+    }
+}
---
+++ MicroservicesClient/MicroservicesClient/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MicroservicesClient.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public bool Ispermanent { get; set; }
+        public Employee()
+        {
+        }
+    }
+}
